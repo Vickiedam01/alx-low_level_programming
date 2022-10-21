ed7d6fd0ae7db543d8e219f58eb7cef14d3fb5ea
Damina Precious <ifeoluwadamina@gmail.com>
2022-10-21 06:34:54 -0700

    modified

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints a square
- * @size: sizeof the square
+ * @size: size of the square
  */
 void print_square(int size)
 {
@@ -16,7 +16,7 @@ void print_square(int size)
 
 		for (i = 0; i < size; i++)
 		{
-			for (j + 0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
